0625a3ac1034c4056d682b4887d7ddf2178eb448
Sixten Otto <sixten@resultsdirect.com>
2022-01-03 16:29:46 -0700

    update to 5.1.3

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "TwitterKit",
-      url: "https://github.com/ResultsDirect/twitter-kit-ios/releases/download/v5.1.2/TwitterKit.xcframework.zip",
-      checksum: "a5cae47f0afc55aef8faf9256917ad244d2521900ea1827efe73c51268da0177"
+      url: "https://github.com/ResultsDirect/twitter-kit-ios/releases/download/v5.1.3/TwitterKit.xcframework.zip",
+      checksum: "b92fab5cdbf0b458cbf3df241250f1237650d68366e62e33e8d0854794c5acbe"
     ),
   ]
 )
